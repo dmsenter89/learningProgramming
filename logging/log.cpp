b0f1e5c01b72ecfc3a65549bce5f090f4793c4c1
Michael Senter <dmsenter89@gmail.com>
2014-10-26 17:13:31 -0600

    Separate Time from Logger
    Wrote a separte function to get the correct time stamp;

--- logging/log.cpp
+++ logging/log.cpp
@@ -1,16 +1,20 @@
 #include <fstream>
 #include <ctime>
 #include <string>
+
+void get_time( std::string &tstring)
+{
+	std::time_t now = time(0);
+	std::string dt( ctime(&now) );
+        tstring = dt.substr(0, dt.length()-1);
+}
  
 void log( const std::string &text )
 {
-	// Get the time stamp
-	std::time_t now = time(0);
-	std::string dt( ctime(&now) );
-	
-	//sTime << t.substr( 0, t.length() -1  );
+    std::string tstring;
+    get_time(tstring);
+
     std::ofstream log_file(
         "log_file.txt", std::ios_base::out | std::ios_base::app );
-    log_file << dt.substr(0, dt.length()-1) 
-				<< ": " << text << std::endl;
+    log_file << tstring  << ": " << text << std::endl;
 }
