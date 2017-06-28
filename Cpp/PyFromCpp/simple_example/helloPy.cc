b2391e3b88eb687c43de3c09dc8e948531a6b4b5
Michael Senter <dmsenter89@gmail.com>
2017-06-28 10:19:19 -0400

    Updated gitignore for CMake

--- Cpp/PyFromCpp/simple_example/helloPy.cc
+++
@@ -1,9 +0,0 @@
-#include <python3.6m/Python.h>
-
-int main()
-{
-    Py_Initialize();
-    PyRun_SimpleString("print(\"Hello, world!\")");
-    Py_Finalize();
-    return 0;
-}
--- Cpp/PyFromCpp/simple_example/mini_make.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-# sample of how to compile for Python 3.6
-
-g++ helloPy.cc -lpython3.6m
